f137bb707703a8c7c9f589ac5aae3deca78e7827
Matt Rota <mrota@stevens.edu>
2018-11-09 17:36:22 -0500

    got the routing to work, working on api calls

--- lab3/src/App.js
+++ lab3/src/App.js
@@ -1,32 +1,75 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+import axios from 'axios';
+import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios';
 
-const App = () => {
+
+
+const App = () => (
   <Router>
     <div>
-      <Header />
+      {/* <Header /> */}
 
       <Route exact path="/" component={Home} />
-      <Route path="/pokemon/page/" component={Pokemen} />
-      <Route path="/pokemon/" component={Pokemon} />
-      <Route path="/berries/page/" component={Berries} />
-      <Route path="/berries/" component={Berry} />
-      <Route path="/machines/page/" component={Machines} />
-      <Route path="/machines/" component={Machine} />
+      <Route path="/pokemon/page/:page" exact component={Pokemen} />
+      <Route path="/pokemon/:id" exact component={Pokemon} />
+      <Route path="/berries/page/:page" exact component={Berries} />
+      <Route path="/berries/:id" exact component={Berry} />
+      <Route path="/machines/page/:page" exact component={Machines} />
+      <Route path="/machines/:id" exact component={Machine} />
     </div>
   </Router>
-};
+);
 
-const Home = () => <h1>Home</h1>;
+const Home = () => (
+  <div>
+    <h1>Welcome to the React Pokedex!</h1>
+    <p>Pokemon are...</p>
+    <Link to="/pokemon/page/0">Link to Pokemen</Link>
+    <p>Berries are...</p>
+    <Link to="/berries/page/0">Link to Berries</Link>
+    <p>Machines are...</p>
+    <Link to="/machines/page/0">Link to Machines</Link>
+  </div>
+);
 
-const Pokemon = ({ match }) =>
-const Pokemen = ({ match }) =>
+const Pokemon = ({ match }) => <h1>Pokemon {match.params.id}</h1>;
+const Pokemen = ({ match }) => (
+  <div>
+    <Get url="https://pokeapi.co/api/v2/pokemon/" params={{}}>
+      {(error, response, isLoading, onReload) => {
+        if(error) {
+          return (
+          <div>
+            Something went wrong! {error.message}
+          </div>
+          );
+        }
+        else if(isLoading) {
+          return (<div>Loading...</div>);
+        }
+        else if (response !== null) {
+          return (<div>{response.data.message}</div>);
+        }
+        return(<div>Pokemon Page {match.params.id}</div>)
+      }}
+    </Get>
+  </div>
+);
 
-const Berry = ({ match }) =>
-const Berries = ({ match }) =>
+const Berry = ({ match }) => <h1>Berry {match.params.id}</h1>;
+const Berries = ({ match }) => (
+  <div>
+    {/* <h1>Berry List: Page {match.params.page}</h1> */}
+  </div>
+);
 
-const Machine = ({ match }) =>
-const Machines = ({ match }) =>
+const Machine = ({ match }) => <h1>Machine {match.params.id}</h1>;
+const Machines = ({ match }) => (
+  <div>
+    <h1>Machine List: Page {match.params.page}</h1>
+  </div>
+);
 
 export default App;
